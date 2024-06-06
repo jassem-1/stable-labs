8f7ae7bbc686339e3d0c9adfb76c4cb133952884
unknown <soueyjassem@gmail.com>
2024-06-06 20:44:20 +0100

    get ether price function with etherscan API

--- app/components/global/Header.tsx
+++ app/components/global/Header.tsx
@@ -1,16 +1,21 @@
-"use client"
-import { useState } from "react";
+"use client";
+import { useEffect } from "react";
 import logo from "../../assets/logo.png";
+import axios from "axios";
 
 const Header: React.FC = () => {
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
-  const handleItemClick = (index: number): void => {
-    setActiveIndex(index);
-  };
-
+  const getEtherPrice = ()  => {
+    try {
+      const API_ETHER_KEY="5FKGRH8CW2C4TIW9ME321HB6XXY53HZZP1"
+      axios.get(`https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${API_ETHER_KEY}`).then(response =>
+      {  console.log(response.data.result) } )
+        
+    } catch (error) { console.log(error); }
+  }
+  useEffect(() => {getEtherPrice()}, []);
   return (
-    <div className='fixed w-full bg-[#172554] shadow-lg h-[48px] z-20'>
+    <div className='fixed w-full bg-[#172554] shadow-lg  z-20'>
       <div className="flex justify-between items-center w-full px-8 py-1">
         <div>
             <img src={logo.src} alt="" className="w-10 h-10"/>
@@ -25,6 +30,15 @@ const Header: React.FC = () => {
               </button>
           </div>
         
+        </div>
+      </div>
+      <div className="flex justify-between items-center w-full ">
+        <div className="flex flex-col text-white">
+        <p>ether price : </p>
+        <p>gas price : </p>
+        </div>
+        <div>
+          <button className="px-4 py-1 bg-white border border-black rounded-2xl ">connect wallet</button>
         </div>
       </div>
     </div>
