117a0b645654a19bd7db569458bcf9af178301dd
unknown <soueyjassem@gmail.com>
2024-06-08 00:54:35 +0100

    fetching transaction details

--- app/transaction/[id]/page.tsx
+++ app/transaction/[id]/page.tsx
@@ -1,30 +1,47 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { EtherscanContext } from '@/app/RootLayoutContext';
+import { ethers } from 'ethers';
+import { useState, useEffect, useContext } from 'react';
 
 const TransactionPage = () => {
-  const [transactionId, setTransactionId] = useState('');
+  const [transactionId, setTransactionId] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+  const { getTransactionDetails, transactionDetails } = useContext(EtherscanContext) ?? {};
 
   useEffect(() => {
     const pathSegments = window.location.pathname.split('/');
     const id = pathSegments[pathSegments.length - 1]; // Get the last segment
-    if (id) {
+    if (id && transactionId !== id) {
       setTransactionId(id);
       console.log("Retrieved Transaction ID:", id);
+      if (getTransactionDetails) {
+        getTransactionDetails(id).then(() => setLoading(false));
+      }
     } else {
       console.log("No Transaction ID found");
+      setLoading(false);
     }
-  }, []);
+  }, [getTransactionDetails]);
 
-  if (!transactionId) {
+  if (loading) {
     return <div>Loading...</div>;
   }
 
+  if (!transactionDetails) {
+    return <div>No transaction details available.</div>;
+  }
+
+
   return (
     <div>
-      <h1>Transaction Hash: {transactionId}</h1>
-      {/* Additional transaction details can be displayed here */}
-    </div>
+    <h1>Transaction Hash: {transactionId}</h1>
+    <p>Block: {transactionDetails.blockNumber}</p>
+    <p>From: {transactionDetails.from}</p>
+    <p>To: {transactionDetails.to}</p>
+    <p>Value: {ethers.formatEther(transactionDetails.value)} ETH</p>
+    <p>Gas Price: {ethers.formatUnits(transactionDetails.gasPrice, 'gwei')} Gwei</p>
+  </div>
   );
 };
 
